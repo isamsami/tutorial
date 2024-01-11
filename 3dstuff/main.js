34a886e5cd802eb264520df4408c3b9515afb78e
isamsami <samisalah.am@gmail.com>
2024-01-10 20:24:28 -0500

    added navbar and suntexture

--- 3dstuff/main.js
+++ 3dstuff/main.js
@@ -1,15 +1,21 @@
 import * as THREE from 'three';
 import './style.css'
+import gsap from 'gsap';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
 
 //scene
 const scene = new THREE.Scene()
 
-//make sphere
-const geometry = new THREE.SphereGeometry(3, 64, 64)
+//texture for planets
+const texture = new THREE.TextureLoader().load('8k_sun.jpg')
+
+//make sun
+const sungeo = new THREE.SphereGeometry(3, 32, 32)
 const material = new THREE.MeshStandardMaterial({
-  color: "#00ff83",
+  map: texture
 })
-const mesh = new THREE.Mesh(geometry, material)
+const mesh = new THREE.Mesh(sungeo, material)
 scene.add(mesh)
 
 //sizes of webpage
@@ -29,11 +35,18 @@ const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 
 camera.position.z = 20
 scene.add(camera)
 
+
 //renderer
 const canvas = document.querySelector(".webgl")
 const renderer = new THREE.WebGLRenderer({ canvas })
 renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
+
+//controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+controls.autoRotate = true
+controls.autoRotateSpeed = 7
 
 //resize window
 window.addEventListener("resize", () => {
@@ -46,9 +59,22 @@ window.addEventListener("resize", () => {
   renderer.setSize(sizes.width, sizes.height)
 })
 
-//resize object
+
+//loop through resize and update
 const loop = () => {
+  controls.update()
   renderer.render(scene, camera)
   window.requestAnimationFrame(loop)
 }
 loop()
+
+//timeline magic
+const tl = gsap.timeline({defaults: {duration: 1 }})
+tl.fromTo("nav", {y: "-100%"}, {y: "0%"})
+tl.fromTo(mesh.scale, {z: 0, x: 0, y: 0}, {z: 1, x: 1, y: 1})
+
+
+//mouse animation color
+let mouseDown = false
+window.addEventListener("mouseDown", () => (mouseDown = true))
+window.addEventListener("mouseup", () => (mouseDown = false))
